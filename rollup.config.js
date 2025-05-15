15944ea7c4003f1e2c7d50170cda57a9542616ab
Skycat1983 <hlaoutaris@gmail.com>
2025-05-15 10:21:53 +0200

    added config and made chanegs tyo accomodate rollup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default [
+  // Background Script Bundle
+  {
+    input: "src/scripts/background/index.ts",
+    output: {
+      file: "dist/scripts/background.js",
+      format: "iife",
+    },
+    plugins: [typescript(), resolve(), commonjs()],
+  },
+  // Content Script Bundle
+  {
+    input: "src/scripts/content.ts",
+    output: {
+      file: "dist/scripts/content.js",
+      format: "iife",
+    },
+    plugins: [typescript(), resolve(), commonjs()],
+  },
+  // Popup Script Bundle
+  {
+    input: "src/scripts/popup/index.ts",
+    output: {
+      file: "dist/scripts/popup.js",
+      format: "iife",
+    },
+    plugins: [typescript(), resolve(), commonjs()],
+  },
+];
