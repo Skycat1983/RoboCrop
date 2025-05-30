36ee6a2c237e6539dd81ab6f9ffa562a27ab526e
Skycat1983 <hlaoutaris@gmail.com>
2025-05-30 10:57:01 +0200

    removed unused background script files

--- src/scripts/background/constants.ts
+++
@@ -1,5 +0,0 @@
-export const constants = {
-  num1: 1,
-  num2: 2,
-  num3: 3,
-};
--- src/scripts/background/index.ts
+++
@@ -1,5 +0,0 @@
-import { constants } from "./constants";
-import { sum } from "./sum";
-
-// console.log(constants);
-// console.log(sum(constants.num1, constants.num2));
